f04c9b26f03376637ac04ab66743c2e4153c5179
hammed01 <shotolahammed01@gmail.com>
2018-05-29 16:43:23 +0100

    added Navigation bar for mobile phones

--- script/script.js
+++ script/script.js
@@ -1,21 +1,50 @@
     $(function(){
-        document.getElementById('showform').addEventListener('click', function(){
-            document.getElementById('Form').style.visibility='visible';
-        });
+       
+        $('.nav-bar').click(function(){
+    var nav = $('.main-nav');
+    var icon = $('.nav-bar i');
+    
+    nav.slideToggle(200);
+    if (icon.hasClass('fa-bars'))
+{
+    icon.addClass('fa-times');
+    icon.removeClass('fa-bars');
 
+}else{
+     icon.addClass('fa-bars');
+    icon.removeClass('fa-times');
+    
+}})
+        $('.about').click(function(){
+    $('html, body').animate({scrollTop: $('.about-me').offset().top}, 1000);    
+    });
+      $('.arrow-down').click(function(){
+    $('html, body').animate({scrollTop: $('.about-me').offset().top}, 1000);    
+    });
 
-      var $dom = document.getElementById('Form');
+    $('.mywork').click(function(){
+    $('html, body').animate({scrollTop: $('.my-work').offset().top}, 1000);    
+    });
+    
+    $(function(){
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
 
-        var $window = $(window);
-        $window.on('scroll', function(){
-
-            if ($window.scrollTop() <= 300){
-                $dom.style.visibility='hidden';
-
-            }else{
-
-            }
-        })
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+    
+});
+        
+        
     });
 
     
